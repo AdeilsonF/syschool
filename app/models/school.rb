552af748d3bf2a5ed76f227aae0af27c52d678de
AdeilsonF <adeilsono.oinfo@gmail.com>
2017-01-18 19:32:26 -0200

    Add model school

---
+++ app/models/school.rb
@@ -0,0 +1,6 @@
+class School < ApplicationRecord
+  validates :name, :pitch, :subdomain, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
+end
